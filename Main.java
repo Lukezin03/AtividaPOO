e43cd224b936bbd35c93e608529bc48ca6675bf5
Lukezin03 <117870645+Lukezin03@users.noreply.github.com>
2023-11-17 21:44:33 -0300

    Atualização 2.0

--- Main.java
+++ Main.java
@@ -2,24 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-class Aluno
-{
+class Aluno{
   private String nome;
   private List < Disciplina > disciplinas;
 
-  public Aluno (String nome)
-  {
+  public String getNome(){
+    return nome;
+  }
+
+  public Aluno (String nome){
     this.nome = nome;
     this.disciplinas = new ArrayList <> ();
   }
 
-  public void adicionarDisciplina (Disciplina disciplina)
-  {
+  public void adicionarDisciplina (Disciplina disciplina){
     disciplinas.add (disciplina);
   }
 
-  public void imprimir() 
-  {
+  public void imprimir(){
     System.out.println("Aluno: " + nome);
     System.out.println("Disciplinas:");
     for (Disciplina disciplina : disciplinas) {
@@ -30,24 +30,24 @@ class Aluno
 }
 
 
-class Curso
-{
+class Curso{
   private String nome;
   private List < Disciplina > disciplinas;
 
-  public Curso (String nome)
-  {
+  public String getNome(){
+    return nome;
+  }
+
+  public Curso (String nome){
     this.nome = nome;
     this.disciplinas = new ArrayList <> ();
   }
 
-  public void adicionarDisciplina (Disciplina disciplina)
-  {
+  public void adicionarDisciplina (Disciplina disciplina){
     disciplinas.add (disciplina);
   }
 
-  public void imprimir() 
-  {
+  public void imprimir() {
     System.out.println("Curso: " + nome);
     System.out.println("Disciplinas:");
     for (Disciplina disciplina : disciplinas) {
@@ -58,24 +58,24 @@ class Curso
 }
 
 
-class Professor
-{
+class Professor{
   private String nome;
   private List < Disciplina > disciplinas;
 
-  public Professor (String nome)
-  {
+  public String getNome(){
+    return nome;
+  }
+
+  public Professor (String nome){
     this.nome = nome;
     this.disciplinas = new ArrayList <> ();
   }
 
-  public void adicionarDisciplina (Disciplina disciplina)
-  {
+  public void adicionarDisciplina (Disciplina disciplina){
     disciplinas.add (disciplina);
   }
 
-  public void imprimir() 
-  {
+  public void imprimir(){
     System.out.println("Professor: " + nome);
     System.out.println("Disciplinas:");
     for (Disciplina disciplina : disciplinas) {
@@ -86,33 +86,32 @@ class Professor
 }
 
 
-class Disciplina
-{
+class Disciplina{
   private String nome;
   private List < Aluno > alunos;
   private List < Curso > cursos;
   private Professor professor;
 
-  public Disciplina (String nome, Professor professor)
-  {
+  public String getNome(){
+    return nome;
+  }
+
+  public Disciplina (String nome, Professor professor){
     this.nome = nome;
     this.professor = professor;
     this.alunos = new ArrayList <> ();
     this.cursos = new ArrayList <> ();
   }
 
-  public void adicionarAluno (Aluno aluno)
-  {
+  public void adicionarAluno (Aluno aluno){
     alunos.add (aluno);
   }
 
-  public void adicionarCurso (Curso curso)
-  {
+  public void adicionarCurso (Curso curso){
     cursos.add (curso);
   }
 
-  public void imprimir() 
-  {
+  public void imprimir(){
     System.out.println("Disciplina: " + nome);
     System.out.println("Professor: " + professor.getNome());
     System.out.println("Alunos:");
@@ -128,10 +127,8 @@ class Disciplina
 }
 
 
-public class Main
-{
-  public static void main (String[]args)
-  {
+public class Main{
+  public static void main (String[]args){
 
     Aluno aluno01 = new Aluno ("Lucas");
     Curso curso01 = new Curso ("Ciencia da Computacao");
